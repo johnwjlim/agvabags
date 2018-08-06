1fc4ac34ce44039f18f0996e69c0c4e2ff7f8179
John Lim <wjlim@uw.edu>
2018-08-06 12:21:55 +0700

    responsiveness to headroom

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,8 @@ import styled from 'styled-components'
 import Headroom from "react-headroom"
 
 const Container = styled.div`
-  background-color: rebeccapurple; `;
+  background-color: rebeccapurple; 
+`;
 
  const Wrapper = styled.div`
   margin: 0 auto;
@@ -17,16 +18,46 @@ const Container = styled.div`
   color: white;
  `;
 
-const Header = ({ siteTitle }) => (
-  <Headroom wrapperStyle={{marginBottom: "1.5rem"}}>
-    <Container>
-      <Wrapper>
-        <Link to ="/" style={{textDecoration: "none"}}>
-          <HeaderText>{siteTitle}</HeaderText>
-        </Link>
-      </Wrapper>
-    </Container>
-  </Headroom>
-)
+ const ResponsiveWrapper = styled(Headroom)`
+  margin-bottom: 1.5rem; 
 
-export default Header
+  @media(max-width: 768px) {
+    display: none;
+  }
+ `;
+
+// const Header = ({ siteTitle }) => (
+//   <Headroom wrapperStyle={{marginBottom: "1.5rem"}}>
+//     <Container>
+//       <Wrapper>
+//         <Link to ="/" style={{textDecoration: "none"}}>
+//           <HeaderText>{siteTitle}</HeaderText>
+//         </Link>
+//       </Wrapper>
+//     </Container>
+//   </Headroom>
+// )
+
+// export default Header
+
+export default class Header extends React.Component {
+  constructor() {
+    super(); 
+  }
+
+  render() {
+    return (
+      <ResponsiveWrapper>
+        <Headroom>
+          <Container>
+            <Wrapper>
+              <Link to ="/" style={{textDecoration: "none"}}>
+                <HeaderText>{this.props.siteTitle}</HeaderText>
+              </Link>
+            </Wrapper>
+          </Container>
+        </Headroom>
+      </ResponsiveWrapper>
+    )
+  }
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Header from './header'
 import './layout.css'
+import './styles.css'
+
 
 const Layout = ({ children, data }) => (
   <StaticQuery
@@ -28,7 +31,7 @@ const Layout = ({ children, data }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header siteTitle={data.site.siteMetadata.title}/>
         <div
           style={{
             margin: '0 auto',
