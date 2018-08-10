18e81a23245e75eee08e3fc20f18c828f47d0de0
John Lim <wjlim@uw.edu>
2018-08-10 10:48:36 +0800

    initialize gatsby node for contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,3 +7,38 @@
 // You can delete this file if you're not using it
 
 const path = require('path')
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+    const {createPage} = boundActionCreators
+    return new Promise((resolve, reject) =>  {
+        const productTemplate = path.resolve('src/templates/product.js')
+        resolve(
+            graphql(`
+                {
+                    allContentfulProduct {
+                        edges {
+                            node {
+                                id
+                                slug
+                            }
+                        }
+                    }
+                }
+            `).then((result) => {
+                if (result.errors) {
+                    reject(result.errors)
+                }
+                result.data.allContentfulProduct.edges.forEach((edge) => {
+                    createPage ({
+                        path: edge.node.slug,
+                        componenet: productTemplate,
+                        context: {
+                            slug: edge.node.slug
+                        }
+                    })
+                })
+                return
+            })
+        )
+    })
+}
